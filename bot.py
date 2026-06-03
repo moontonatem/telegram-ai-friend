c3482022048ca24bc8274ec201617cac76ac39a0
moontonatem <moontonatem@gmail.com>
2026-06-03 15:17:54 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,43 +4,39 @@ from flask import Flask
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
-print("BOT DOSYASI YUKLENDI")
+print("SURUM TEST 2026")
 
 web_app = Flask(__name__)
 
 @web_app.route("/")
 def home():
+    print("WEB ISTEGI GELDI")
     return "Bot Aktif"
 
 def run_web():
     port = int(os.environ.get("PORT", 10000))
+    print("FLASK BASLIYOR PORT:", port)
     web_app.run(host="0.0.0.0", port=port)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    print("/start CALISTI")
-    await update.message.reply_text("Bot aktif çalışıyor ✅")
-
-async def test(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    print("/test CALISTI")
-    await update.message.reply_text("Test başarılı 🚀")
+    print("/START CALISTI")
+    await update.message.reply_text("Bot çalışıyor ✅")
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print("MESAJ:", update.message.text)
-    await update.message.reply_text(
-        f"Yazdığın mesaj: {update.message.text}"
-    )
+    await update.message.reply_text(update.message.text)
 
 def main():
 
+    print("MAIN BASLADI")
+
     token = os.getenv("BOT_TOKEN")
 
-    print("BOT BASLADI")
     print("TOKEN VAR:", bool(token))
 
     application = Application.builder().token(token).build()
 
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("test", test))
     application.add_handler(
         MessageHandler(filters.TEXT & ~filters.COMMAND, echo)
     )
