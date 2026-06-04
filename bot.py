e7b430282bc55b5397f2dc297f99069d153762f2
moontonatem <moontonatem@gmail.com>
2026-06-04 09:47:28 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,10 @@
 import os
+import requests
 from flask import Flask, request
 
 app = Flask(__name__)
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 @app.route("/")
 def home():
@@ -10,15 +13,27 @@ def home():
 @app.route("/webhook", methods=["POST"])
 def webhook():
 
-    print("=" * 50)
-    print("WEBHOOK TETIKLENDI")
-    print("=" * 50)
-
     try:
+
         data = request.get_json()
 
-        print("GELEN VERI:")
+        print("=" * 50)
         print(data)
+        print("=" * 50)
+
+        if "message" not in data:
+            return "ok", 200
+
+        chat_id = data["message"]["chat"]["id"]
+
+        requests.post(
+            f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+            json={
+                "chat_id": chat_id,
+                "text": "Webhook çalışıyor 🚀"
+            },
+            timeout=30
+        )
 
     except Exception as e:
 
@@ -31,8 +46,7 @@ if __name__ == "__main__":
 
     port = int(os.environ.get("PORT", 10000))
 
-    print("WEBHOOK TEST SURUMU")
-    print("PORT:", port)
+    print("WEBHOOK CEVAP TESTI")
 
     app.run(
         host="0.0.0.0",
